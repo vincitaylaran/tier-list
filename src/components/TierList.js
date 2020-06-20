2393ecaa2af13fd8afd4801e3b7cdd4286774e84
Vinci Taylaran <svl.taylaran@gmail.com>
2020-06-20 12:03:36 -0600

    rebuild tier list row

--- src/components/TierList.js
+++ src/components/TierList.js
@@ -1,61 +1,71 @@
 import React, { Component } from "react";
-
-import { initialData } from "../initial-data";
-
-import Table from "./Table";
-import ItemsGrid from "./ItemsGrid";
-
-import { DragDropContext } from "react-beautiful-dnd";
-
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Row = styled.div`
+  width: 100%;
+  background-color: lightgrey;
+
+  display: flex;
+`;
+
+const Value = styled.div`
+  background-color: blue;
+  width: 9%;
+
+  display: flex;
+  justify-content: center;
+`;
+
+const Items = styled.div`
+  display: flex;
+  background-color: orange;
+  width: 89%;
+`;
+
+const Item = styled.div`
+  background-color: red;
+`;
+
+const Actions = styled.div`
+  display: flex;
+  background-color: lightgreen;
+`;
+
+const Action = styled.div``;
 
 class TierList extends Component {
-  state = {
-    rows: initialData.rows,
-    items: initialData.items,
-  };
-
-  onDragEnd = (required) => {
-    const { source, destination } = required;
-
-    // create deep copies of the 'rows' and 'items' in state.
-    let rows = JSON.parse(JSON.stringify(this.state.rows));
-    let items = JSON.parse(JSON.stringify(this.state.items));
-    let item;
-
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index !== destination.index
-    ) {
-      // TODO: write logic to rearrange items if dropped in the same droppable.
-      console.log("is in same droppable");
-
-      return;
-    }
-
-    item = items.gridMain.gridItems.splice(source.index - 1, 1)[0];
-
-    rows.forEach((row) => {
-      if (row.rowId === destination.droppableId) {
-        row.items.splice(destination.index - 1, 0, item);
-      }
-    });
-
-    this.setState({ rows, items });
-  };
-
   render() {
     return (
       <Container>
-        <DragDropContext onDragEnd={this.onDragEnd}>
-          <Table data={this.state.rows} />
-          <ItemsGrid
-            data={this.state.items.gridMain.gridItems}
-            id={this.state.items.gridMain.gridId}
-          />
-        </DragDropContext>
+        <Row>
+          <Value>
+            <h1>S</h1>
+          </Value>
+          <Items>
+            <Item>
+              <h3>Item 1</h3>
+            </Item>
+            <Item>
+              <h3>Item 2</h3>
+            </Item>
+            <Item>
+              <h3>Item 3</h3>
+            </Item>
+          </Items>
+          <Actions>
+            <Action>
+              <h3>A1</h3>
+            </Action>
+            <Action>
+              <h3>A2</h3>
+            </Action>
+          </Actions>
+        </Row>
       </Container>
     );
   }
